5e308fcfce8a0beec277b5c4a456c14b6ad6feea
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-04 19:50:39 +0700

    Setup internal demo

--- app/scripts/ng1-ux-select.controller.js
+++ app/scripts/ng1-ux-select.controller.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   angular
-    .module('')
+    .module('nmt.comp.ng1UxSelect')
     .controller('Ng1UxSelectCtrl', Ng1UxSelectCtrl)
   ;
   Ng1UxSelectCtrl.$inject = [];
---
+++ main.js
@@ -0,0 +1,6 @@
+(function () {
+  'use strict';
+  angular
+    .module('nmt.demo.comp.ng1UxSelect', ['nmt.comp.ng1UxSelect'])
+  ;
+})();
