dc0d107f1b036ba1cb9a474cf8228f0d0402d631
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-10 12:35:12 +0700

    Add lodash library and utilize it to handle simple search functionality

--- app/scripts/ng1-ux-select.controller.js
+++ app/scripts/ng1-ux-select.controller.js
@@ -4,19 +4,29 @@
     .module('nmt.comp.ng1UxSelect')
     .controller('Ng1UxSelectCtrl', Ng1UxSelectCtrl)
   ;
-  Ng1UxSelectCtrl.$inject = [];
+  Ng1UxSelectCtrl.$inject = ['$scope', '$timeout'];
 
-  function Ng1UxSelectCtrl() {
+  function Ng1UxSelectCtrl($scope, $timeout) {
     var vm = this;
+
+    var debouceSearch;
+    var searchTimeoutId;
+    var searchString;
 
     constructor();
 
     function constructor() {
+      // Local variables
+      debouceSearch = _.debounce(startSearching, 250);
+      searchTimeoutId = -1;
+      searchString = '';
+
       // Properties
-      vm.loading = true;
+      vm.loading = false;
 
       // Methods
       vm.onInputFocused = onInputFocused;
+      vm.onKeyPressed = onKeyPressed;
       vm.onToggleDropDown = onToggleDropDown;
     }
 
@@ -24,11 +34,31 @@
       $('.dropdown-toggle').dropdown('toggle');
     }
 
+    function onKeyPressed() {
+      debouceSearch();
+    }
+
     function onToggleDropDown($event) {
       if (vm.loading) {
         $event.preventDefault();
         $event.stopPropagation();
       }
+    }
+
+    function search() {
+      console.log('search');
+      vm.loading = false;
+      vm.searchString = searchString;
+      searchString = '';
+      $timeout.cancel(searchTimeoutId);
+    }
+
+    function startSearching() {
+      searchString = vm.searchString;
+      vm.searchString = 'Loading...';
+      vm.loading = true;
+      $scope.$digest(); // This functon is called inside lodash env, scope must be digested to update the view
+      searchTimeoutId = $timeout(search, 1 * 1000);
     }
   }
 })();
