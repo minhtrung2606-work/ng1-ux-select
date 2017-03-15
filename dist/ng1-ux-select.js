42d3674eb98c79593c6f141a7415d15c55469dad
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-16 02:24:20 +0700

    Dist build

--- dist/ng1-ux-select.js
+++ dist/ng1-ux-select.js
@@ -4,19 +4,111 @@
     .module('nmt.comp.ng1UxSelect')
     .controller('Ng1UxSelectCtrl', Ng1UxSelectCtrl)
   ;
-  Ng1UxSelectCtrl.$inject = [];
+  Ng1UxSelectCtrl.$inject = ['$scope', '$timeout'];
 
-  function Ng1UxSelectCtrl() {
+  var Item = (function () {
+    function Item(name) {
+      this.name = name || 'Unknown';
+      this.prop1 = 'Value of Prop 1 Value of Prop 1';
+      this.prop2 = 'Value of Prop 2 Value of Prop 2';
+    }
+
+    return Item;
+  })();
+
+  function Ng1UxSelectCtrl($scope, $timeout) {
     var vm = this;
 
+    var selectedItem;
+    var debouceSearch;
+    var itemList;
+    var searchTimeoutId;
+    var searchString;
+
     constructor();
+    onInit();
 
     function constructor() {
+      // Local variables
+      debouceSearch = _.debounce(startSearching, 500);
+      searchTimeoutId = -1;
+      searchString = '';
+
+      // Properties
+      vm.loading = false;
+
+      // Methods
+      vm.getItemDesc = getItemDesc;
       vm.onInputFocused = onInputFocused;
+      vm.onItemClicked = onItemClicked;
+      vm.onKeyPressed = onKeyPressed;
+      vm.onToggleDropDown = onToggleDropDown;
+    }
+
+    function getItemDesc(item) {
+      var props = vm.descPaths.split(',');
+      var itemDesc = [];
+      _.each(props, function getItemPropValue(prop) {
+        itemDesc.push(_.get(item, prop));
+      });
+      return itemDesc.join('; ');
+    }
+
+    function onInit() {
+      vm.descPaths = 'prop1,prop2';
+      vm.itemList = [
+        new Item('Item 1'),
+        new Item('Item 2'),
+        new Item('Item 3'),
+        new Item('Item 4'),
+        new Item('Item 5')
+      ];
     }
 
     function onInputFocused() {
-      $('.dropdown-toggle').dropdown('toggle');
+      if (!vm.isDropdownOpened()) {
+        $('.dropdown-toggle').dropdown('toggle');
+      }
+    }
+
+    function onItemClicked(item) {
+      if (selectedItem) {
+        selectedItem.selected = false;
+      }
+
+      item.selected = true;
+      vm.searchString = item.name;
+      selectedItem = item;
+    }
+
+    function onKeyPressed() {
+      debouceSearch();
+    }
+
+    function onToggleDropDown($event) {
+      if (vm.loading) {
+        $event.preventDefault();
+        $event.stopPropagation();
+      }
+    }
+
+    function search() {
+      vm.loading = false;
+      vm.searchString = searchString;
+      searchString = '';
+      vm.itemList = itemList;
+      itemList = [];
+      $timeout.cancel(searchTimeoutId);
+    }
+
+    function startSearching() {
+      searchString = vm.searchString;
+      vm.searchString = 'Loading...';
+      itemList = vm.itemList;
+      vm.itemList = [];
+      vm.loading = true;
+      $scope.$digest(); // This functon is called inside lodash env, scope must be digested to update the view
+      searchTimeoutId = $timeout(search, 1 * 1000);
     }
   }
 })();
@@ -33,15 +125,23 @@
     var template = [
       '<div class="ng1-ux-select-comp">',
       '  <div class="input-group dropdown">',
-      '    <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" ng-focus="vm.onInputFocused()">',
+      '    <input ng-model="vm.searchString" ng-keypress="vm.onKeyPressed()" ng-readonly="vm.loading" type="text" class="form-control" aria-label="Amount (to the nearest dollar)" ng-focus="vm.onInputFocused()">',
       '    <ul class="dropdown-menu" aria-labelledby="ng1-ux-select-dropdown-label">',
-      '      <li><a href="#">Action</a></li>',
-      '      <li><a href="#">Another action</a></li>',
-      '      <li><a href="#">Something else here</a></li>',
-      '      <li role="separator" class="divider"></li>',
-      '      <li><a href="#">Separated link</a></li>',
+      '      <li ng-repeat="item in vm.itemList track by $index"',
+      '          ng-click="vm.onItemClicked(item)">',
+      '        <a ng-class="{\'selected\': item.selected}">',
+      '          <span><strong>{{item.name}}</strong></span><br/>',
+      '          <span><small>{{vm.getItemDesc(item)}}</small></span>',
+      '        </a>',
+      '      </li>',
+      '      <li class="disabled" ng-if="!vm.loading && (!vm.itemList || vm.itemList.length === 0)">',
+      '        <a><small><dfn>No item existing</dfn></small></a>',
+      '      </li>',
+      '      <li ng-if="vm.loading">',
+      '        <a><small>Loading...</small></a>',
+      '      </li>',
       '    </ul>',
-      '    <span class="input-group-addon dropdown-toggle" role="button" id="ng1-ux-select-dropdown" data-toggle="dropdown"',
+      '    <span ng-click="vm.onToggleDropDown($event)" class="input-group-addon dropdown-toggle" role="button" id="ng1-ux-select-dropdown" data-toggle="dropdown"',
       '          aria-haspopup="true" aria-expanded="true"><span class="caret"></span></span>',
       '  </div>',
       '</div>'
@@ -53,7 +153,23 @@
       template: template.join(''),
       controller: 'Ng1UxSelectCtrl',
       controllerAs: 'vm',
-      bindToController: {}
+      bindToController: {},
+      link: function (scope, elem) {
+        var vm = scope.vm;
+        var ngElem = angular.element(elem);
+        var ngDropDownElem = ngElem.find('.input-group.dropdown');
+
+        if (vm) {
+          vm.isDropdownOpened = isDropdownOpened;
+        }
+
+        isDropdownOpened();
+
+        function isDropdownOpened() {
+          var cssClass = ngDropDownElem.attr('class');
+          return cssClass && cssClass.indexOf('open') > -1;
+        }
+      }
     };
   }
 })();
