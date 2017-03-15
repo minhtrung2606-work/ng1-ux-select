f04559024b994bdf796b03cc931c394ccc0770fc
Nguyen Minh Trung <minhtrung2606.work@outlook.com>
2017-03-16 02:03:28 +0700

    Add item list. Update dropdown layout

--- app/scripts/ng1-ux-select.controller.js
+++ app/scripts/ng1-ux-select.controller.js
@@ -6,14 +6,26 @@
   ;
   Ng1UxSelectCtrl.$inject = ['$scope', '$timeout'];
 
+  var Item = (function () {
+    function Item(name) {
+      this.name = name || 'Unknown';
+      this.prop1 = 'Value of Prop 1 Value of Prop 1';
+      this.prop2 = 'Value of Prop 2 Value of Prop 2';
+    }
+
+    return Item;
+  })();
+
   function Ng1UxSelectCtrl($scope, $timeout) {
     var vm = this;
 
     var debouceSearch;
+    var itemList;
     var searchTimeoutId;
     var searchString;
 
     constructor();
+    onInit();
 
     function constructor() {
       // Local variables
@@ -25,16 +37,35 @@
       vm.loading = false;
 
       // Methods
+      vm.getItemDesc = getItemDesc;
       vm.onInputFocused = onInputFocused;
       vm.onKeyPressed = onKeyPressed;
       vm.onToggleDropDown = onToggleDropDown;
     }
 
+    function getItemDesc(item) {
+      var props = vm.descPaths.split(',');
+      var itemDesc = [];
+      _.each(props, function getItemPropValue(prop) {
+        itemDesc.push(_.get(item, prop));
+      });
+      return itemDesc.join('; ');
+    }
+
+    function onInit() {
+      vm.descPaths = 'prop1,prop2';
+      vm.itemList = [
+        new Item('Item 1'),
+        new Item('Item 2'),
+        new Item('Item 3'),
+        new Item('Item 4'),
+        new Item('Item 5')
+      ];
+    }
+
     function onInputFocused() {
       if (!vm.isDropdownOpened()) {
         $('.dropdown-toggle').dropdown('toggle');
-      } else {
-        console.log('Already opened');
       }
     }
 
@@ -50,16 +81,19 @@
     }
 
     function search() {
-      console.log('search');
       vm.loading = false;
       vm.searchString = searchString;
       searchString = '';
+      vm.itemList = itemList;
+      itemList = [];
       $timeout.cancel(searchTimeoutId);
     }
 
     function startSearching() {
       searchString = vm.searchString;
       vm.searchString = 'Loading...';
+      itemList = vm.itemList;
+      vm.itemList = [];
       vm.loading = true;
       $scope.$digest(); // This functon is called inside lodash env, scope must be digested to update the view
       searchTimeoutId = $timeout(search, 1 * 1000);
